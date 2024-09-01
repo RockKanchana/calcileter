af0f8210ebb618fcc86fc0335d6af806904ce9f9
RockKanchana <rockkanchana2003@gmail.com>
2024-09-01 18:11:38 +0530

    Add files via upload

---
+++ calcileter/script.js
@@ -0,0 +1,12 @@
+function calculateAge() {
+    var dob = new Date(document.getElementById("dob").value);
+    var today = new Date();
+    var age = today.getFullYear() - dob.getFullYear();
+    var monthDiff = today.getMonth() - dob.getMonth();
+    
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+        age--;
+    }
+    
+    document.getElementById("result").innerHTML = "You are " + age + " years old.";
+}
